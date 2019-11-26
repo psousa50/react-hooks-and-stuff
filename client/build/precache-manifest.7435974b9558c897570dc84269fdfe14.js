832604999aa8825a5f6d984ee812bb68ad0be3b2
Pedro Sousa <psousa@equalexperts.com>
2019-11-26 09:58:39 +0000

    Refactor project structure - Add client folder

---
+++ client/build/precache-manifest.7435974b9558c897570dc84269fdfe14.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2e7139096f61fd8444f30175d7c78f4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a7bfe7428f2af643e282",
+    "url": "/static/js/2.aec3a22f.chunk.js"
+  },
+  {
+    "revision": "6b6a62d136dec2c390ef",
+    "url": "/static/js/main.f11f7daf.chunk.js"
+  },
+  {
+    "revision": "6587ae3b71e76e59896b",
+    "url": "/static/js/runtime-main.4cfdfeac.js"
+  }
+]);
